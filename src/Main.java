c8ed93e9f10d2f85692ed70304abef7f11ab5bec
Lucas Simões <ssl.lucas.simoes@gmail.com>
2020-05-07 19:12:29 -0300

    updated server

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,13 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
-
+    public static void printMenu(){
+        System.out.println("+-------+------------------------------------------+");
+        System.out.println("| CON   |   Fazer consulta de desenho armazenados. |");
+        System.out.println("| SAV   |   Salvar desenho.                        |");
+        System.out.println("| FIC   |   Sair do programa.                      |");
+        System.out.println("+-------+------------------------------------------+");
+    }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         Scanner scanner = new Scanner(System.in);
@@ -15,6 +21,33 @@ public class Main {
         Operacao operacao = (Operacao) objectInputStream.readObject();
         OperacoesCommander operacoesCommander = new OperacoesCommander(operacao, new OperacoesImpl());
         operacoesCommander.executa();
-    }
 
+//        Scanner scanner = new Scanner(System.in);
+//        try {
+//            ServerSocket request = new ServerSocket (12345);
+//            Socket conn = request.accept();
+//        } catch (Exception erro){
+//            System.out.println(erro.getMessage());
+//        }
+//        System.out.println("O servidor está ativo.");
+//        printMenu();
+//
+//        for(;;){
+//            String comando = "";
+//            try{
+//                System.out.print("> ");
+//                comando = scanner.nextLine();
+//
+//                Operacao op = null;
+//                switch(comando){
+//                    case "CON":
+//                        Ope
+//                        break;
+//                    case "SAV":
+//                        String nome_desenho;
+//                }
+//
+//            }catch (Exception erro){}
+//        }
+    }
 }
